1c1e2a5b886e6ab94b806f36d13480e8eb99164f
cokeefe <chaemobile@gmail.com>
2013-06-04 10:23:21 -0400

    update sort

--- 10chpt_sorting.rb
+++ 10chpt_sorting.rb
@@ -1,20 +1,8 @@
-def sort some_array
-  recursive_sort some_array
-end
-
 def recursive_sort unsorted_array, sorted_array
-end
-
-def nonrecursive_sort unsorted_array, sorted_array
-  until unsorted_array.empty?
-    sorted_array << unsorted_array.shift
-    sorted_array.map {|worda, wordb| worda <=> wordb}  
-    #compare items in array
-    #"king of the hill" - a if a > b
-     
+  # sorted_array is []
+  return if unsorted_array.empty?
+  unsorted_array.each do |word| 
+    max_word = word if word > max_word
+    sorted_array << max_word
   end
 end
-
-puts arry1 = ["a", "z", "e"]
-puts arry2 = ["b", "c", "d", "f"]
-nonrecursive_sort arry1, arry2
