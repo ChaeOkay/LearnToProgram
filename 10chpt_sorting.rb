aa4f8a1946d90925f4f7ba2ddd75018a90b67393
cokeefe <chaemobile@gmail.com>
2013-06-04 11:01:39 -0400

    update sort

--- 10chpt_sorting.rb
+++ 10chpt_sorting.rb
@@ -1,8 +1,16 @@
+# sorted_array is []
+
 def recursive_sort unsorted_array, sorted_array
-  # sorted_array is []
-  return if unsorted_array.empty?
+  max_word = 0
   unsorted_array.each do |word| 
-    max_word = word if word > max_word
+    return if word == "nil"
+    max_word = word > max_word ? word : max_word
     sorted_array << max_word
-  end
+    end
+  sorted_array
 end
+
+unsorted_array = [1, 5, 8, 3, 5]
+recursive_sort unsorted_array, []
+
+# wondering if I'm allowed to use .sort <=>
